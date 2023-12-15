53a035f52a702dc1edf35bb6b9ca8cd0da896094
Chris Newland <chrisn@advfn.com>
2023-12-15 21:35:30 +0000

    Day 15 part 2

--- src/main/java/com/chrisnewland/aoc2023/Day15.java
+++ src/main/java/com/chrisnewland/aoc2023/Day15.java
@@ -2,54 +2,183 @@ package com.chrisnewland.aoc2023;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Day15
 {
-    public static void main(String[] args) throws Exception
-    {
-        new Day15();
-    }
+	public static void main(String[] args) throws Exception
+	{
+		new Day15();
+	}
 
-    //        Determine the ASCII code for the current character of the string.
-//        Increase the current value by the ASCII code you just determined.
-//        Set the current value to itself multiplied by 17.
-//        Set the current value to the remainder of dividing itself by 256.
-    private long hash(String input)
-    {
-        long result = 0;
+	private int hash(String input)
+	{
+		int result = 0;
 
-        int len = input.length();
+		int len = input.length();
 
-        for (int i = 0; i < len; i++)
-        {
-            char c = input.charAt(i);
+		for (int i = 0; i < len; i++)
+		{
+			char c = input.charAt(i);
 
-            result += (int) c;
-            result *= 17;
-            result = result % 256;
-        }
+			result += (int) c;
+			result *= 17;
+			result = result % 256;
+		}
 
-        System.out.println("Hash of " + input +" = " + result);
+		return result;
+	}
 
-        return result;
-    }
+	private static class Lens
+	{
+		private String label;
+		private int focalLength;
 
-    public Day15() throws Exception
-    {
-        List<String> lines = Files.readAllLines(Paths.get("src/main/resources/day15.txt"));
+		public Lens(String label, int focalLength)
+		{
+			this.label = label;
+			this.focalLength = focalLength;
+		}
 
-        String line = lines.get(0);
+		@Override
+		public String toString()
+		{
+			return "Lens{" + "label='" + label + '\'' + ", focalLength=" + focalLength + '}';
+		}
+	}
 
-        String[] parts = line.split(",");
+	private static class Box
+	{
+		private int number;
 
-        long sum = 0;
+		private List<Lens> lenses = new LinkedList<>();
 
-        for (String part : parts)
-        {
-            sum += hash(part);
-        }
+		public Box(int number)
+		{
+			this.number = number;
+		}
 
-        System.out.println("Part 1 hash: " + sum);
-    }
+		public void addLens(Lens lens)
+		{
+			Iterator<Lens> iter = lenses.iterator();
+
+			boolean replaced = false;
+
+			while (iter.hasNext())
+			{
+				Lens existing = iter.next();
+
+				if (existing.label.equals(lens.label))
+				{
+					existing.focalLength = lens.focalLength;
+					replaced = true;
+				}
+			}
+
+			if (!replaced)
+			{
+				lenses.add(lens);
+			}
+		}
+
+		public void removeLens(String label)
+		{
+			Iterator<Lens> iter = lenses.iterator();
+
+			while (iter.hasNext())
+			{
+				Lens existing = iter.next();
+
+				if (existing.label.equals(label))
+				{
+					iter.remove();
+				}
+			}
+		}
+
+		public int getPower()
+		{
+			int power = 0;
+
+			for (int i = 0; i < lenses.size(); i++)
+			{
+				power += (1 + number) * (i + 1) * lenses.get(i).focalLength;
+			}
+
+			return power;
+		}
+
+		@Override
+		public String toString()
+		{
+			return "Box{" + "number=" + number + ", lenses=" + lenses + '}';
+		}
+	}
+
+	public Day15() throws Exception
+	{
+		List<String> lines = Files.readAllLines(Paths.get("src/main/resources/day15.txt"));
+
+		String line = lines.get(0);
+
+		String[] parts = line.split(",");
+
+		System.out.println("Part 1 hash: " + doPart1(parts));
+
+		System.out.println("Part 2 power: " + doPart2(parts));
+	}
+
+	private long doPart1(String[] parts)
+	{
+		long sum = 0;
+
+		for (String part : parts)
+		{
+			sum += hash(part);
+		}
+
+		return sum;
+	}
+
+	private long doPart2(String[] parts)
+	{
+		Box[] boxes = new Box[256];
+
+		for (int i = 0; i < boxes.length; i++)
+		{
+			boxes[i] = new Box(i);
+		}
+
+		for (String part : parts)
+		{
+			if (part.contains("-"))
+			{
+				String[] boxParts = part.split("-");
+				String label = boxParts[0];
+				int boxNumber = hash(label);
+				Box box = boxes[boxNumber];
+				box.removeLens(label);
+			}
+			else if (part.contains("="))
+			{
+				String[] boxParts = part.split("=");
+				String label = boxParts[0];
+				int focalLength = Integer.parseInt(boxParts[1]);
+				int boxNumber = hash(label);
+				Box box = boxes[boxNumber];
+				box.addLens(new Lens(label, focalLength));
+			}
+		}
+
+		long power = 0;
+
+		for (int i = 0; i < boxes.length; i++)
+		{
+			power += boxes[i].getPower();
+		}
+
+		return power;
+	}
 }
